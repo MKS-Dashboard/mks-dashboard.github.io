b64b9a2f93ba40b24a00a42d12cd4087b6a88204
Piet2001 <piet2001@fireman.net>
2021-11-22 22:05:26 +0100

    fix footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <div className="footer">
             <p className="footer-text">
-                © Copyright 2021 | MKS-Dashboard | <img src="https://tokei.rs/b1/github/piet2001/mks-dashboard" alt='SLOC'></img>
+                © Copyright 2021 | MKS-Dashboard | <img src="https://tokei.rs/b1/github/MKS-Dashboard/mks-dashboard.github.io" alt='SLOC'></img>
             </p>
         </div>
     );
