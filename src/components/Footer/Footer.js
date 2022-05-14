82707f0b1f5bd1d605953a8e60cabe51ea40056a
Piet2001 <54183168+Piet2001@users.noreply.github.com>
2022-05-14 17:50:22 +0200

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <div className="footer">
             <p className="footer-text">
-                © Copyright 2021 {new Date().getFullYear()} | MKS-Dashboard | <img src="https://tokei.rs/b1/github/MKS-Dashboard/mks-dashboard.github.io" alt='SLOC'></img> | <a href="/voorwaarden">Algemene Voorwaarden</a> en <a href="/privacy">Privacyverklaring</a>
+                © Copyright 2021 - {new Date().getFullYear()} | MKS-Dashboard | <img src="https://tokei.rs/b1/github/MKS-Dashboard/mks-dashboard.github.io" alt='SLOC'></img> | <a href="/voorwaarden">Algemene Voorwaarden</a> en <a href="/privacy">Privacyverklaring</a>
             </p>
         </div>
     );
