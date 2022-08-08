963ea771eebcbea49749e9ea9df6556190c35799
Piet2001 <piet2001@fireman.net>
2022-08-08 13:13:30 +0200

    fix #120

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,13 +11,47 @@ import React from "react";
 
 const Header = () => {
   return (
+    // <div className="header-wrapper">
+    //   <Navbar className="navbar" expand="lg">
+    //     <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    //     <Navbar.Collapse id="basic-navbar-nav">
+    //       <Nav className="ml-auto">
+    //         <Link className="link" to="/">
+    //           <FaHome id="icon" size={50} />
+    //         </Link>
+    //         <Link className="link" to="/vehicles">
+    //           <RiPoliceCarFill id="icon" size={50} />
+    //         </Link>
+    //         <Link className="link" to="/buildings">
+    //           <FaBuilding id="icon" size={50} />
+    //         </Link>
+    //         <Link className="link" to="/alliancebuildings">
+    //           <TiGroup id="icon" size={50} />
+    //         </Link>
+    //         <Link className="link" to="/progressdata">
+    //           <BsClipboardData id="icon" size={50} />
+    //         </Link>
+    //         <Link className="link" to="/credits">
+    //           <BiWorld id="icon" size={50} />
+    //         </Link>
+    //         <Link className="link" to="/suggestions">
+    //           <FcMultipleInputs id="icon" size={50} />
+    //         </Link>
+    //       </Nav>
+    //     </Navbar.Collapse>
+    //   </Navbar>
+    // </div>
+
     <div className="header-wrapper">
       <Navbar className="navbar" expand="lg">
+        <Link className="link" to="/">
+          <Navbar.Brand>
+            <FaHome id="icon" size={50} />
+          </Navbar.Brand>
+        </Link>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <Link className="link" to="/">
-              <FaHome id="icon" size={50} />
-            </Link>
             <Link className="link" to="/vehicles">
               <RiPoliceCarFill id="icon" size={50} />
             </Link>
@@ -39,7 +73,7 @@ const Header = () => {
           </Nav>
         </Navbar.Collapse>
       </Navbar>
-    </div>
+    </div >
   );
 };
 
