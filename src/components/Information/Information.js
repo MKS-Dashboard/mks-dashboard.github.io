32f9be1ff02df6d17e03abb91d0c4f4781a529cd
Piet2001 <54183168+Piet2001@users.noreply.github.com>
2022-09-05 14:12:07 +0200

    Update standaard informatie pagina

--- src/components/Information/Information.js
+++ src/components/Information/Information.js
@@ -2,7 +2,8 @@ import React from 'react'
 
 function Information() {
     return (
-        <div id="Container">Welkom in het informatie gedeelte. Deze pagina is tijdelijk.</div>
+        <div id="Container">Welkom in het informatie gedeelte. In de dropdown kunnen verschillende vormen van informatie worden gevonden.<br />
+        Heb je bepaalde informatie die je hier graag wilt zien? Meld deze via de suggestiebox en misschien voegen we de informatie spoedig toe.</div>
     )
 }
 
