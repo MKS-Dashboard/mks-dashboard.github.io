42c0842a6c7c114ab4bed37153dfca5bfea07c46
Piet2001 <54183168+Piet2001@users.noreply.github.com>
2026-02-11 20:37:25 +0100

    Update API URL for production environment

--- src/App.js
+++ src/App.js
@@ -44,7 +44,7 @@ function App() {
 
 
   if (window.location.href.includes("localhost") || window.location.href.includes("netlify")) {
-    apiUrl = "https://mks-dashboard-backend-dev-y1p5.onrender.com"
+    apiUrl = "https://mks-dashboard-backend-prd-production.up.railway.app"
   }
   else {
     apiUrl = "https://mks-dashboard-backend-prd.onrender.com"
